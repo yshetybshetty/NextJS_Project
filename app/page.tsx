0cdd6318179a0242459f942f7c3fed5036643be5
Yashwanth.b <yshety.b@gmail.com>
2026-05-17 22:47:27 +0530

    jhk

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from "@/app/ui/home.module.css"
-import { lusitanaFont } from '@/app/ui/font';
+import { lusitana } from '@/app/ui/font';
 import Image from 'next/image';
 
 export default function Page() {
   return (
-    <main className={`flex min-h-screen flex-col p-6 ${lusitanaFont.className}`}>
+    <main className={`flex min-h-screen flex-col p-6 ${lusitana.className}`}>
       <div className={styles.shape}>
         <AcmeLogo />
       </div>
--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,10 +1,10 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitanaFont } from '@/app/ui/font';
+import { lusitana } from '@/app/ui/font';
 
 export default function AcmeLogo() {
   return (
     <div
-      className={`${lusitanaFont.className} flex flex-row items-center leading-none text-white`}
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
       <p className="text-[44px]">Acme</p>
